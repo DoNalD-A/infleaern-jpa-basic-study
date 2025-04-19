f7516fc2677d19c3331e57189c9b7638d47280f9
DoNalD-A <thdlstjrj@naver.com>
2025-04-20 00:20:30 +0900

    섹션3. JPA 시작하기

---
+++ src/main/java/jpabasic/hellojpa/JpaMain.java
@@ -0,0 +1,37 @@
+package jpabasic.hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+        try {
+//            Member findMember = entityManager.find(Member.class, 2L);
+            List<Member> result = entityManager.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member name = " + member.getName());
+            }
+
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        entityManagerFactory.close();
+    }
+}
---
+++ src/main/java/jpabasic/hellojpa/Member.java
@@ -0,0 +1,28 @@
+package jpabasic.hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id //PK가 무엇인지는 알려줘야 한다
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
